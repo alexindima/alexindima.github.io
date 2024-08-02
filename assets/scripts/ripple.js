c06d7f9be11bdd0e838a2f342fa01fc893595d25
AlexinD <dalexin@codemastersintl.com>
2024-08-02 21:07:46 +0400

    Add ripple effect. Some fixes

---
+++ assets/scripts/ripple.js
@@ -0,0 +1,25 @@
+const rippledElements = document.querySelectorAll(".ripple-effect");
+
+rippledElements.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        const rect = element.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+
+        let ripple = document.createElement("span");
+        ripple.classList.add("ripple-effect--span");
+        ripple.style.left = `${x}px`;
+        ripple.style.top = `${y}px`;
+
+        element.appendChild(ripple);
+
+        const removeRipple = (e) => {
+            if (e.target === ripple) {
+                ripple.removeEventListener("animationend", removeRipple);
+                ripple.remove();
+            }
+        };
+        ripple.addEventListener("animationend", removeRipple);
+    });
+});
