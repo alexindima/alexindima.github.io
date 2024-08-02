6b15c207aff95971e0ec2162e8b960fbf8e5de69
AlexinD <dalexin@codemastersintl.com>
2024-08-03 00:50:45 +0400

    Add ability to edit document

---
+++ assets/scripts/custom/editable.js
@@ -0,0 +1,58 @@
+function getXPath(element) {
+    if (element.id) {
+        return "id(\"" + element.id + "\")";
+    }
+    if (element === document.body) {
+        return element.tagName;
+    }
+
+    let ix = 0;
+    const siblings = element.parentNode.childNodes;
+    for (let i = 0; i < siblings.length; i++) {
+        const sibling = siblings[i];
+        if (sibling === element) {
+            return getXPath(element.parentNode) + "/" + element.tagName + "[" + (ix + 1) + "]";
+        }
+        if (sibling.nodeType === 1 && sibling.tagName === element.tagName) {
+            ix++;
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const editableElements = document.querySelectorAll("[contenteditable='true']");
+    const resetButton = document.getElementById("reset-button");
+
+    editableElements.forEach(element => {
+        const path = getXPath(element);
+        const savedContent = localStorage.getItem(path);
+        if (savedContent) {
+            element.innerHTML = savedContent;
+        }
+    });
+
+    editableElements.forEach(element => {
+        element.addEventListener("input", () => {
+            const path = getXPath(element);
+            localStorage.setItem(path, element.innerHTML);
+            toggleResetButton();
+        });
+    });
+
+    resetButton.addEventListener("click", () => {
+        localStorage.clear();
+        location.reload();
+    });
+
+    const hasChanges = () => {
+        return Array.from(editableElements).some(element => {
+            const path = getXPath(element);
+            return localStorage.getItem(path) !== null;
+        });
+    };
+
+    const toggleResetButton = () => {
+        resetButton.style.display = hasChanges() ? "block" : "none";
+    };
+    toggleResetButton();
+});
